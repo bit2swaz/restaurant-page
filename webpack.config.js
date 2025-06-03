d2da5a45743a6c04e56e71870c3b320bcf6f8f1a
Aditya <bit2swaz@gmail.com>
2025-06-03 13:16:08 +0000

    chore: setup webpack and .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
